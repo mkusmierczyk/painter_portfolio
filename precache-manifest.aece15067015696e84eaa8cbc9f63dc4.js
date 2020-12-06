47b5e33d4056cf4cff331b143d0b40d3e308e086
Mateusz <makusmierczyk@gmail.com>
2020-12-06 19:36:00 +0100

    Updates

---
+++ precache-manifest.aece15067015696e84eaa8cbc9f63dc4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1ed529158d3ee963e1811e512104b9",
+    "url": "/painter_portfolio/index.html"
+  },
+  {
+    "revision": "2c78259fb1c1840854df",
+    "url": "/painter_portfolio/static/css/main.f891afc5.chunk.css"
+  },
+  {
+    "revision": "7fb744ae1e9246e6eb43",
+    "url": "/painter_portfolio/static/js/2.aa6e94fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/painter_portfolio/static/js/2.aa6e94fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c78259fb1c1840854df",
+    "url": "/painter_portfolio/static/js/main.bbcb612c.chunk.js"
+  },
+  {
+    "revision": "f9481ab02d00d3dfd87b",
+    "url": "/painter_portfolio/static/js/runtime-main.95cc4673.js"
+  },
+  {
+    "revision": "1a9020c337be1b3a6556473307bb9a9f",
+    "url": "/painter_portfolio/static/media/envelope-regular.1a9020c3.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/painter_portfolio/static/media/facebook-square-brands.34e73505.svg"
+  },
+  {
+    "revision": "79eead8ed3b7e8b6fced73a775d64f0a",
+    "url": "/painter_portfolio/static/media/image0.79eead8e.jpeg"
+  },
+  {
+    "revision": "8723102ad0c16142426c00120842078c",
+    "url": "/painter_portfolio/static/media/image1.8723102a.jpeg"
+  },
+  {
+    "revision": "fbab8bd7520a735514342138f8d700fe",
+    "url": "/painter_portfolio/static/media/image2.fbab8bd7.jpeg"
+  },
+  {
+    "revision": "79eead8ed3b7e8b6fced73a775d64f0a",
+    "url": "/painter_portfolio/static/media/image3.79eead8e.jpeg"
+  },
+  {
+    "revision": "bb83ef052b08b025f7837c91b14777c8",
+    "url": "/painter_portfolio/static/media/image4.bb83ef05.jpeg"
+  },
+  {
+    "revision": "da833bac34fd69e4b25123336e0e9608",
+    "url": "/painter_portfolio/static/media/image5.da833bac.jpeg"
+  },
+  {
+    "revision": "26449437b601ad6cd185315d37ee2f2b",
+    "url": "/painter_portfolio/static/media/image6.26449437.jpeg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/painter_portfolio/static/media/instagram-brands.e75317fa.svg"
+  },
+  {
+    "revision": "8d77becb409ba5412804bc57cefe1bb9",
+    "url": "/painter_portfolio/static/media/palette-solid.8d77becb.svg"
+  },
+  {
+    "revision": "429f86e358c09ad055346784b688eda3",
+    "url": "/painter_portfolio/static/media/pencil-ruler-solid.429f86e3.svg"
+  },
+  {
+    "revision": "efb5734b9db5e20049dbacb2cf34d32b",
+    "url": "/painter_portfolio/static/media/quote-left-solid.efb5734b.svg"
+  },
+  {
+    "revision": "b47090721c7a338b198b4f07141283c4",
+    "url": "/painter_portfolio/static/media/tumblr-square-brands.b4709072.svg"
+  }
+]);
